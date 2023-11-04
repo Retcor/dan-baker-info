7d94098f6e9f3fe1a983d9fb07bd6528a634810d
BakerRang <bakerz2009@gmail.com>
2023-11-04 15:16:21 -0600

    Update to get nginx build to work properly to work with Google Cloud Run

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,11 @@ RUN npm install
 COPY . .
 RUN npm run build
 
-# Stage 2: Create a lightweight production image from the build files
-FROM nginx:alpine
+# Stage 2: Use Nginx to serve the built app
+FROM nginx:stable-alpine
+
 COPY --from=build /app/dist /usr/share/nginx/html
-EXPOSE 80
+COPY nginx/nginx.conf /etc/nginx/conf.d/default.conf
+EXPOSE 8080
 
 CMD ["nginx", "-g", "daemon off;"]
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Needed for the Docker Container port mapping to work IF we have the Dockerfile doing "npm run dev".
+    // Otherwise, the build will just create the index.html that nginx.conf takes care of
+    host: true,
+    strictPort: true,
+    port: 8080,
+  },
 })
