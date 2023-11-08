e07cc7459f250ee5dd3a0a9a628d65b9436f3133
BakerRang <bakerz2009@gmail.com>
2023-11-07 17:20:56 -0700

    About verbiage changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,9 +19,8 @@ const About = () => {
       >
         I'm a full stack software developer with experience in a range of different web development technologies, like Java, Kotlin, TypeScript,
         JavaScript, Node.js, and other libraries and frameworks like React, Redux, Spring, KTOR, Hibernate, Express and Tailwind CSS.
-        I'm a quick learner and although I may lack knowledge a company is looking forward, I will pick up new technologies
-        quickly by building out personal projects, which is precisely what this website was to help me pick up Kubernetes, Docker, React and Tailwind CSS,
-        as this is deployed in Google Cloud Platform using their Kubernetes engine, with a React frontend and Node backend.<br/><br/>
+        I'm a quick learner who picks up new technologies quickly by building out personal projects. In fact, this website was an opportunity
+        to learn Kubernetes, Docker, React and Tailwind CSS, as this is deployed in Google Cloud Platform using their Kubernetes engine.<br/><br/>
         I love meeting new people and do very well working with people from all walks of life and look forward to hearing from you!
       </motion.p>
     </>
